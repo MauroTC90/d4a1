49992fb383f2a70ecd6aa365dd1ff254e3246314
d4admin <d4admin@d4a>
2019-04-25 13:15:08 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+
+<?php
+$enlace = mysql_connect('mysql', 'root', 'TM1q2w3e.');
+if (!$enlace) {
+    die('Could not connect: ' . mysql_error());
+}
+echo 'Connecteros soccsessfoly';
+mysql_close($enlace);
+?>
+
